56a8389d8cb3ce8c2b152aeea54e7028e0d7863c
tshiamomahloko <tshiahloks@gmail.com>
2021-01-19 13:00:33 +0200

    Added Project page and also made a few changes then added social media pages on the contact page.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-    });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+  });
 
-    $('.menu-item').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-item i').toggleClass("active");
-    });
+  $(".menu-item").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-item i").toggleClass("active");
+  });
 });
